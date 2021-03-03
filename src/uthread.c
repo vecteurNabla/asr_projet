cbf781f975b92c6152c09169f048b1aacc95ef3e
Guillaume  Gatt <guillaume.gatt@ens-lyon.fr>
2021-03-03 11:20:58 +0100

    la version avec des arguments

--- src/uthread.c
+++ src/uthread.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 #include "uthread.h"
 //make_vcpu : créer un processeur virtuel
-int make_vcpu(int){}
+int make_vcpu(int n_vcpu){}
 /*create_uthread : créer un thread
 qui lance une fonction avec arguments*/
-uthread_t create_uthread(void*(void*),void*){}
+uthread_t create_uthread(void*(void*) f,void* args){}
 //permet de spécifier le quantum en secondes
-int set_quantum(int){}
+int set_quantum(int quantum){}
 //commence tout les threads
 int start_uthreads(){}
 //quitte le thread courant (on met au début de la stack l'id du thread)
